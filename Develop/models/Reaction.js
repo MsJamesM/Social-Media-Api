3f7ba8f382bdb48103b373470512606b6150b9bd
MsJamesM <118479305+MsJamesM@users.noreply.github.com>
2023-07-20 16:16:52 -0600

    adding base code to /models files

--- Develop/models/Reaction.js
+++ Develop/models/Reaction.js
@@ -1,2 +1,23 @@
 const { Schema, Types } = require("mongoose");
-const { Schema, model } = require("mongoose");
+
+const reactionSchema = new Schema({
+  reactionId: {
+    /* Use Mongoose's ObjectId data type. Default value is set to a new ObjectId */
+  },
+  reactionBody: {
+    type: String,
+    required: true,
+    maxlength: 280,
+  },
+  username: {
+    type: String,
+    required: true,
+  },
+  createdAt: {
+    type: Date,
+    default: Date.now,
+    /* Set default value to the current timestamp. Use a getter method to format the timestamp on query */
+  },
+});
+
+module.exports = reactionSchema;
--- Develop/models/Thought.js
+++ Develop/models/Thought.js
@@ -1,1 +1,34 @@
 const { Schema, model } = require("mongoose");
+
+const thoughtSchema = new Schema(
+  {
+    thoughtText: {
+      type: String,
+      required: true,
+      minLength: 1,
+      maxLength: 280,
+    },
+    createdAt: {
+      type: Date,
+      default: Date.now(),
+      /* Set default value to the current timestamp. Use a getter method to format the timestamp on query */
+    },
+    username: {
+      type: String,
+      required: true,
+    },
+    reactions: {
+      /* Array of nested documents created with the `reactionSchema` */
+    },
+  },
+  {
+    toJSON: {
+      virtuals: true,
+    },
+    id: false,
+  }
+);
+
+const Thought = model("thought", thoughtSchema);
+
+module.exports = Thought;
