86e221460a93b31ace85155dbd3f5797e7d7a401
MsJamesM <118479305+MsJamesM@users.noreply.github.com>
2023-07-20 16:39:16 -0600

    adding comments and base codes to /controllers and /api files

---
+++ Develop/controllers/thoughtController.js
@@ -0,0 +1,22 @@
+const { Thought, User } = require("../models");
+
+module.exports = {
+    // get all
+    async getThought(req, res)
+}
+
+    // get one
+async getThoughtByID(req, res) {
+}
+
+    // post thought
+async createThought(req, res) {
+}
+
+    // put thought by ID
+async updateThoughtByID(req, res) {
+}
+
+    // delete thought by ID
+async deleteThoughtByID(req, res) {
+}
---
+++ Develop/controllers/userController.js
@@ -0,0 +1,22 @@
+const { User, Thought } = require("../models");
+
+module.exports = {
+    // get all
+    async getUsers(req, res)
+}
+
+    // get one
+async getUserByID(req, res) {
+}
+
+    // post user
+async createUser(req, res) {
+}
+
+    // put user
+async updateUser(req, res) {
+}
+
+    // delete user
+async deleteUser(req, res) {
+}
