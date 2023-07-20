4fa8aa61b0673bdb4e51592c4049d0b01304f431
MsJamesM <118479305+MsJamesM@users.noreply.github.com>
2023-07-20 16:57:36 -0600

    updating /controllers files

--- Develop/controllers/thoughtController.js
+++ Develop/controllers/thoughtController.js
@@ -1,22 +1,77 @@
-const { Thought, User } = require("../models");
+const { Thought } = require("../models");
 
 module.exports = {
-    // get all
-    async getThought(req, res)
-}
+  // get all
+  async getThoughts(req, res) {
+    try {
+      const thoughts = await Thought.find();
+      res.json(thoughts);
+    } catch (err) {
+      res.status(500).json(err);
+    }
+  },
 
-    // get one
-async getThoughtByID(req, res) {
-}
+  // get one
+  async getThoughtByID(req, res) {
+    try {
+      const thought = await Thought.findOne({
+        _id: req.params.thoughtId,
+      }).select("-__v");
 
-    // post thought
-async createThought(req, res) {
-}
+      if (!thought) {
+        return res.status(404).json({ message: "No thought with that ID" });
+      }
 
-    // put thought by ID
-async updateThoughtByID(req, res) {
-}
+      res.json(thought);
+    } catch (err) {
+      res.status(500).json(err);
+    }
+  },
 
-    // delete thought by ID
-async deleteThoughtByID(req, res) {
-}
+  // post thought
+  async createThought(req, res) {
+    try {
+      const thought = await Thought.create(req.body);
+      res.json(thought);
+    } catch (err) {
+      console.log(err);
+      return res.status(500).json(err);
+    }
+  },
+
+  // put thought by ID
+  async updateThought(req, res) {
+    try {
+      const thought = await Thought.findOneAndUpdate(
+        { _id: req.params.thoughtId },
+        { $set: req.body },
+        { runValidators: true, new: true }
+      );
+
+      if (!thought) {
+        return res.status(404).json({ message: "No thought with this id!" });
+      }
+
+      res.json(thought);
+    } catch (err) {
+      res.status(500).json(err);
+    }
+  },
+
+  // delete thought by ID
+  async deleteThought(req, res) {
+    try {
+      const thought = await Thought.findOneAndDelete({
+        _id: req.params.thoughtId,
+      });
+
+      if (!thought) {
+        return res.status(404).json({ message: "No thought with that ID" });
+      }
+
+      res.json(thought);
+    } catch (err) {
+      res.status(500).json(err);
+    }
+  },
+};
--- Develop/controllers/userController.js
+++ Develop/controllers/userController.js
@@ -1,22 +1,75 @@
 const { User, Thought } = require("../models");
 
 module.exports = {
-    // get all
-    async getUsers(req, res)
-}
+  // get all
+  async getUsers(req, res) {
+    try {
+      const users = await User.find();
+      res.json(users);
+    } catch (err) {
+      res.status(500).json(err);
+    }
+  },
 
-    // get one
-async getUserByID(req, res) {
-}
+  // get one
+  async getUserByID(req, res) {
+    try {
+      const user = await User.findOne({ _id: req.params.userId }).select(
+        "-__v"
+      );
 
-    // post user
-async createUser(req, res) {
-}
+      if (!user) {
+        return res.status(404).json({ message: "No user with that ID" });
+      }
 
-    // put user
-async updateUser(req, res) {
-}
+      res.json(user);
+    } catch (err) {
+      res.status(500).json(err);
+    }
+  },
 
-    // delete user
-async deleteUser(req, res) {
-}
+  // post user
+  async createUser(req, res) {
+    try {
+      const user = await User.create(req.body);
+      res.json(user);
+    } catch (err) {
+      console.log(err);
+      return res.status(500).json(err);
+    }
+  },
+
+  // put user
+  async updateUser(req, res) {
+    try {
+      const user = await User.findOneAndUpdate(
+        { _id: req.params.userId },
+        { $set: req.body },
+        { runValidators: true, new: true }
+      );
+
+      if (!user) {
+        return res.status(404).json({ message: "No user with this id!" });
+      }
+
+      res.json(user);
+    } catch (err) {
+      res.status(500).json(err);
+    }
+  },
+
+  // delete user by id
+  async deleteUser(req, res) {
+    try {
+      const user = await User.findOneAndDelete({ _id: req.params.userId });
+
+      if (!user) {
+        return res.status(404).json({ message: "No user with that ID" });
+      }
+
+      res.json(user);
+    } catch (err) {
+      res.status(500).json(err);
+    }
+  },
+};
